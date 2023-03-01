3022c48ba5e8d0804886bea78ed0d777978925f4
W83w <ya.belov-andreq@yandex.ru>
2023-03-01 13:41:21 +0400

    it's cold- gryach

--- books/КрестикиНоликиС_ИИ_1/main.py
+++ books/КрестикиНоликиС_ИИ_1/main.py
@@ -3,9 +3,6 @@
 import random
 
 def drawBoard(board):
-
-
-
     print(board[7] + '|' + board[8] + '|' + board[9])
     print('-+-+-')
     print(board[4] + '|' + board[5] + '|' + board[6])
@@ -13,21 +10,16 @@ def drawBoard(board):
     print(board[1] + '|' + board[2] + '|' + board[3])
 
 def inputPlayerLetter():
-
-
     letter = ''
     while not (letter == 'X' or letter == 'O'):
         print('Вы выбираете X или O')
         letter = input().upper()
-
-
         if letter == 'X':
             return ['X', 'O']
         else:
             return ['O', 'X']
 
 def whoGoesFirst():
-
     if random.randint(0, 1) == 0:
         return 'Компьютер'
     else:
@@ -38,7 +30,6 @@ def makeMove(border, letter, move):
 
 def isWinner(bo, le):
 
-
     return((bo[7] == le and bo[8] == le and bo[9] == le) or
            (bo[4] == le and bo[5] == le and bo[6] == le) or
            (bo[1] == le and bo[2] == le and bo[3] == le) or
@@ -48,44 +39,42 @@ def isWinner(bo, le):
            (bo[7] == le and bo[5] == le and bo[3] == le) or
            (bo[9] == le and bo[5] == le and bo[1] == le))
 
-def getBoardCopy(board):
 
+def getBoardCopy(board):
     boardCopy = []
     for i in board:
         boardCopy.append(i)
     return boardCopy
 
-def isSpaceFree(board, move):
 
+def isSpaceFree(board, move):
     return board[move] == ' '
 
-def getPlayerMove(board):
 
+def getPlayerMove(board):
     move = ''
     while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
         print('Ваш следующий ход? (1-9)')
         move = input()
     return int(move)
 
+
 def chooseRandomMoveFromList(board, movesList):
     possibleMoves = []
     for i in movesList:
         if isSpaceFree(board, i):
             possibleMoves.append(i)
-
     if len(possibleMoves) != 0:
         return random.choice(possibleMoves)
     else:
         return None
 
-def getComputerMove(board, computerLetter):
 
+def getComputerMove(board, computerLetter):
     if computerLetter == 'X':
         playerLetter = 'O'
     else:
         playerLetter = 'X'
-
-
 
     for i in range(1, 10):
         boardCopy = getBoardCopy(board)
@@ -94,7 +83,6 @@ def getComputerMove(board, computerLetter):
             if isWinner(boardCopy, computerLetter):
                 return i
 
-
     for i in range(1, 10):
         boardCopy = getBoardCopy(board)
         if isSpaceFree(boardCopy, i):
@@ -102,21 +90,16 @@ def getComputerMove(board, computerLetter):
             if isWinner(boardCopy, playerLetter):
                 return i
 
-
     move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
     if move != None:
         return move
-
-
     if isSpaceFree(board, 5):
         return 5
-
-
     return chooseRandomMoveFromList(board, [2, 4, 6, 8])
 
-def isBoaedFull(board):
 
-    for i in  range(1, 10):
+def isBoaedFull(board):
+    for i in range(1, 10):
         if isSpaceFree(board, i):
             return False
     return True
@@ -125,16 +108,13 @@ def isBoaedFull(board):
 print('Игра "Крестики-нолики"')
 
 while True:
-
     theBoard = [' '] * 10
     playerLetter, computerLetter = inputPlayerLetter()
     turn = whoGoesFirst()
-#    print('', + turn + ' ходит первым.')
     gameIsPlaying = True
 
     while gameIsPlaying:
         if turn == 'Человек':
-
             drawBoard(theBoard)
             move = getPlayerMove(theBoard)
             makeMove(theBoard, playerLetter, move)
@@ -150,9 +130,7 @@ while True:
                     break
                 else:
                     turn = 'Компьютер'
-
         else:
-
             move = getComputerMove(theBoard, computerLetter)
             makeMove(theBoard, computerLetter, move)
 
